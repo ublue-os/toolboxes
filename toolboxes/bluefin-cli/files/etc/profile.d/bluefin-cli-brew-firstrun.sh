42dba98f773dbce8f635f4795fbcc8f71766831c
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-01-20 14:22:22 -0500

    fix: bluefin-cli-brew-firstrun.sh for no volume

--- toolboxes/bluefin-cli/files/etc/profile.d/bluefin-cli-brew-firstrun.sh
+++ toolboxes/bluefin-cli/files/etc/profile.d/bluefin-cli-brew-firstrun.sh
@@ -1,11 +1,13 @@
 if test "$(id -u)" -gt "0" && test ! -f /etc/linuxbrew.firstrun; then
   echo "First Run Setup"
-  if test -d /home/linuxbrew; then
+  if test ! -d /home/linuxbrew; then
+    mv /home/homebrew /home/linuxbrew
+  else
     echo "Getting newest repo version of brew"
     su-exec root cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
-    echo "Making sure linuxbrew is owned by ${USER}"
-    su-exec root chown -R $UID /home/linuxbrew
   fi
+  echo "Making sure linuxbrew is owned by ${USER}"
+  su-exec root chown -R $UID /home/linuxbrew
   echo "Setting up sudo for ${USER}"
   echo "#${UID} ALL = (root) NOPASSWD:ALL" | su-exec root tee -a /etc/sudoers > /dev/null
   su-exec root touch /etc/linuxbrew.firstrun
