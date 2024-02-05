43afd733d2d862e689faa54cb29778d66df1bc76
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-02-05 14:38:38 -0500

    feat: bluefin-cli modern unix utils
    feat: make Wolfi have a fake sudo
    
    feat: have linuxbrew in it's own directory by containername

--- toolboxes/bluefin-cli/files/etc/profile.d/00-bluefin-cli-brew-firstrun.sh
+++ toolboxes/bluefin-cli/files/etc/profile.d/00-bluefin-cli-brew-firstrun.sh
@@ -6,29 +6,41 @@ if test "$(id -u)" -gt "0"; then
   export HOMEBREW_REPOSITORY
   if test ! -f /etc/linuxbrew.firstrun; then
     printf "\nBluefin-CLI First Run Setup\n\n"
-    printf "Setting up sudo for ${bold}${USER}${normal}...\t\t\t "
+    printf "Setting up sudo for %s%s%s...\t\t\t " "${bold}" "${USER}" "${normal}"
     echo "#${UID} ALL = (root) NOPASSWD:ALL" | su-exec root tee -a /etc/sudoers > /dev/null
-    printf "${green}[ OK ]${normal}\n"
+    printf "%s[ OK ]%s\n" "${green}" "${normal}"
   fi
 
   if test ! -d /home/linuxbrew/.linuxbrew; then
+    name=""
+    test -f /run/.containerenv && . /run/.containerenv
+    test -f /run/.dockerenv && . /run/.dockerenv
+    linuxbrew_home="${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli/"${name}"
     printf "Setting up Linuxbrew...\t\t\t\t "
-    if test ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli; then
-      mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli
+    if test ! -d "${linuxbrew_home}"; then
+      mkdir -p "${linuxbrew_home}"
+      if test -d "${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli/.linuxbrew; then
+        mv "${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli/.linuxbrew "${linuxbrew_home}"/.linuxbrew
+      fi
     fi
     if test ! -d /home/linuxbrew; then
       su-exec root mkdir -p /home/linuxbrew
     fi
-    su-exec root mount --bind "${XDG_DATA_HOME:-$HOME/.local/share}"/bluefin-cli /home/linuxbrew
+    su-exec root mount --bind "${linuxbrew_home}" /home/linuxbrew
     su-exec root cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
     su-exec root chown -R $UID /home/linuxbrew
-    printf "${green}[ OK ]${normal}\n"
+    unset linuxbrew_home
+    printf "%s[ OK ]%s\n" "${green}" "${normal}"
   fi
+
   if test ! -d /usr/local/share/bash-completion/completions; then
     printf "Setting up Tab-Completions...\t\t\t "
     su-exec root mkdir -p /usr/local/share/bash-completion
     su-exec root mount --bind /run/host/usr/share/bash-completion /usr/local/share/bash-completion
-    printf "${green}[ OK ]${normal}\n"
+    if test -x /run/host/usr/bin/ujust; then
+      su-exec root ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/ujust
+    fi
+    printf "%s[ OK ]%s\n" "${green}" "${normal}"
   fi
 
   if test ! -f /etc/linuxbrew.firstrun; then
