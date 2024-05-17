f7f468d1e8f32e66a3e84ffc5435f0cf1f26fdf8
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-05-17 16:47:49 -0400

    feat: switch wolfi to sudo

--- toolboxes/bluefin-cli/files/etc/profile.d/00-bluefin-cli-brew-firstrun.sh
+++ toolboxes/bluefin-cli/files/etc/profile.d/00-bluefin-cli-brew-firstrun.sh
@@ -3,12 +3,6 @@ if test "$(id -u)" -gt "0"; then
   blue=$(printf '\033[38;5;32m')
   bold=$(printf '\033[1m')
   normal=$(printf '\033[0m')
-  if test ! -f /etc/linuxbrew.firstrun; then
-    printf "\nBluefin-CLI First Run Setup\n\n"
-    printf "Setting up sudo for %s%s%s%s...\t\t\t " "$bold" "$blue" "$USER" "$normal"
-    echo "#$(id -u) ALL = (root) NOPASSWD:ALL" | su-exec root tee -a /etc/sudoers > /dev/null
-    printf "%s[ OK ]%s\n" "${blue}" "${normal}"
-  fi
 
   if test ! -d /home/linuxbrew/.linuxbrew; then
     name="$(hostname -s)"
@@ -21,27 +15,27 @@ if test "$(id -u)" -gt "0"; then
       fi
     fi
     if test ! -d /home/linuxbrew; then
-      su-exec root mkdir -p /home/linuxbrew
+      sudo mkdir -p /home/linuxbrew
     fi
-    su-exec root mount --bind "${linuxbrew_home}" /home/linuxbrew
-    su-exec root cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
-    su-exec root chown -R "$(id -u)" /home/linuxbrew
+    sudo mount --bind "${linuxbrew_home}" /home/linuxbrew
+    sudo cp -R /home/homebrew/.linuxbrew /home/linuxbrew/
+    sudo chown -R "$(id -u)" /home/linuxbrew
     unset linuxbrew_home
     printf "%s[ OK ]%s\n" "${blue}" "${normal}"
   fi
 
   if test ! -d /usr/local/share/bash-completion/completions; then
     printf "Setting up Tab-Completions...\t\t\t "
-    su-exec root mkdir -p /usr/local/share/bash-completion
-    su-exec root mount --bind /run/host/usr/share/bash-completion /usr/local/share/bash-completion
+    sudo mkdir -p /usr/local/share/bash-completion
+    sudo mount --bind /run/host/usr/share/bash-completion /usr/local/share/bash-completion
     if test -x /run/host/usr/bin/ujust; then
-      su-exec root ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/ujust
+      sudo ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/ujust
     fi
     printf "%s[ OK ]%s\n" "${blue}" "${normal}"
   fi
 
   if test ! -f /etc/linuxbrew.firstrun; then
-    su-exec root touch /etc/linuxbrew.firstrun
+    sudo touch /etc/linuxbrew.firstrun
     printf "\nBluefin-CLI first run complete!\n\n"
   fi
 fi
